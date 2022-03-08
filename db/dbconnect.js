17a9a47deb91f1ec387fa47c1123aac02afc9d2d
chamathr <chamatht20@gmail.com>
2022-03-08 14:12:24 +0530

    changes

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -1,12 +1,25 @@
 const {createConnection, Connection} = require('typeorm');
 
+// const connection = async () => await createConnection({
+//     type: "mysql",
+//     host: "localhost",
+//     port: 3306,
+//     username: "root",
+//     password: "Kumar_123",
+//     database: "prisma_test",
+//     synchronize: true,
+//     entities: [
+//         require("../entity/User")
+//     ]
+// })
+
 const connection = async () => await createConnection({
     type: "mysql",
-    host: "localhost",
+    host: "mysql-server-rec.mysql.database.azure.com",
     port: 3306,
-    username: "root",
+    username: "chamath",
     password: "Kumar_123",
-    database: "prisma_test",
+    database: "users-reg",
     synchronize: true,
     entities: [
         require("../entity/User")
